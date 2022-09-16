c81aa8aa2c0cf9320c2066f6c7f63a288839bc44
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-09-16 17:15:24 +0300

    add todo list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,16 +15,44 @@
 // ];
 // ===================================================
 
-export function App() {
-  return (
-    <>
-      {/* <PageTitle textTitle="24th Core Worlds Coalition Conference" /> */}
-      {/* <EventList events={upcomingEvents} /> */}
-      {/* ======================================= */}
-      {/* <Dropdown /> */}
-      {/* ========================================== */}
-      {/* <ColorPicker options={colorPickerOptions} /> */}
-      {/* ========================================== */}
-    </>
-  );
+import React, { Component } from 'react';
+import InitialTodos from 'todos.json';
+import TodoList from './TodoList/TodoList';
+
+export default class App extends Component {
+  state = {
+    todos: InitialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
+
+    return (
+      <>
+        {/* <PageTitle textTitle="24th Core Worlds Coalition Conference" /> */}
+        {/* <EventList events={upcomingEvents} /> */}
+        {/* ======================================= */}
+        {/* <Dropdown /> */}
+        {/* ========================================== */}
+        {/* <ColorPicker options={colorPickerOptions} /> */}
+        {/* ========================================== */}
+        <h1>Состояние компонента</h1>
+        <p>Общее количество: {totalTodoCount}</p>
+        <p>Количество выполненных: {completedTodoCount}</p>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
 }
